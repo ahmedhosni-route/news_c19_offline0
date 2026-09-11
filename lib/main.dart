c1a22999615d181ff960dc7d738e5b7090d6badf
ahmed <ahmedhosni803@gmail.com>
2026-09-11 18:10:50 +0300

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:news_c19_offline/modules/splash/splash_screen.dart';
 
 void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  MyApp();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Ahmed(
+    return MaterialApp(
       home: SplashScreen(),
     );
   }
