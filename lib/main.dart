e3d6915274ed0d964515b97e11644472920885e6
ahmedhosni-route <ahmedhosni.route@gmail.com>
2026-09-11 18:07:41 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:news_c19_offline/modules/splash/splash_screen.dart';
 
 void main() {
-  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
